18a35f535c604070fc6a91a6e41dbf1e7b445cbd
Cyrus Malekpour <cmalekpour@gmail.com>
2017-02-11 17:14:58 -0500

    Unfinished commit for basics of dialogue system

--- TBD/Assets/Scripts/SystemsScripts/DialogueHolder.cs
+++ TBD/Assets/Scripts/SystemsScripts/DialogueHolder.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class DialogueHolder : MonoBehaviour {
 
-    public string dialogue;
+    private int dialogueState;
+	private bool hasDialogueStateBeenSet = false;
     private DialogueManager dMan;
-    public string[] dialogueLines;
+	public string dialogueFile;
+	private List<string> dialogueLines = new List<string>();
+	private Dictionary<string, int> dialogueLabels = new Dictionary<string, int>();
 
 	// Use this for initialization
 	void Start () {
         dMan = FindObjectOfType<DialogueManager>();
+		loadDialogue (dialogueFile);
+	}
+
+	void loadDialogue(string file) {
+		StreamReader r = new StreamReader (file);
+		string line = r.ReadLine();
+		int pos = 0;
+		while (line != null) {
+
+			// Check if this is a label or a line
+			if (line [0] == '\t') {
+				// Regular line
+				dialogueLines.Add(line.Trim());
+				pos++;
+			} else {
+				// Label
+				dialogueLabels.Add(line.Trim(), pos);
+			}
+			line = r.ReadLine ();
+		}
+
+		dialogueLabels.TryGetValue ("start", out dialogueState);
 	}
 	
 	// Update is called once per frame
@@ -26,8 +52,10 @@ public class DialogueHolder : MonoBehaviour {
             {
                 Debug.Log("Calling into Dialogue Manager");
                 dMan.dialogueLines = dialogueLines;
-                dMan.currentLine = 0;
+                dMan.dialogueState = dialogueState;
+				dMan.dialogueLabels = dialogueLabels;
 				dMan.initialFrame = true;
+				dMan.hasDialogueStateBeenSet = hasDialogueStateBeenSet;
                 dMan.ShowDialogue();
             }
         }
--- TBD/Assets/Scripts/SystemsScripts/DialogueManager.cs
+++ TBD/Assets/Scripts/SystemsScripts/DialogueManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,9 +11,12 @@ public class DialogueManager : MonoBehaviour {
     public Text dText;
     public PlayerMovement player;
     public bool dialogueActive = false;
-    public string[] dialogueLines;
-    public int currentLine;
+	public List<string> dialogueLines;
+	public Dictionary<string, int> dialogueLabels;
+	public int dialogueState;
 	public bool initialFrame;
+	public bool hasDialogueStateBeenSet;
+	public bool dialogueEnd;
 
     // Use this for initialization
     void Start()
@@ -23,22 +28,64 @@ public class DialogueManager : MonoBehaviour {
     void Update()
     {
         if (dialogueActive) {
-            dText.text = dialogueLines[currentLine];
 			if (Input.GetKeyUp(KeyCode.Space) && !initialFrame) {
-                Debug.Log("Pressed Space");
-                currentLine++;
-                if (currentLine >= dialogueLines.Length)
+                dialogueState++;
+				if (dialogueState >= dialogueLines.Count || dialogueEnd)
                 {
                     dialogueActive = false;
+					dialogueEnd = false;
                     dBox.SetActive(false);
                     player.frozen = false;
-                    currentLine = 0;
+
+					// Dialogue is over, see if we need to reset the dialogue state
+					if (!hasDialogueStateBeenSet) {
+						dialogueLabels.TryGetValue ("start", out dialogueState);
+					}
                 }
-                dText.text = dialogueLines[currentLine];
+				string output = ParseDialogueLine (dialogueState);
+                dText.text = output;
             }
 			initialFrame = false;
         }
     }
+
+	public string ParseDialogueLine(int dialogueState) {
+		string line = dialogueLines[dialogueState];
+		string ret = "";
+
+		int pos = 0;
+		while (pos < line.Length) {
+			int start = line.IndexOf ("{", pos);
+			if (start == -1) {
+				if (pos < line.Length) {
+					ret = ret + line.Substring (pos);
+					pos = line.Length;
+				}
+			} else {
+				// Get the current token and act on it
+				ret = ret + line.Substring (pos, Math.Max(start - pos - 1, 0));
+				pos = line.IndexOf ("}", start);
+				string tokenstr = line.Substring(start + 1, pos - start - 1);
+
+				// Process the token string
+				switch (tokenstr)
+				{
+				case "end":
+					dialogueEnd = true;
+					break;
+				case "goto":
+					dialogueState = Int32.Parse (tokenstr.Split (new Char[] {':'}) [1]);
+					break;
+				default:
+					break;
+				}
+				Debug.Log (tokenstr);
+			}
+		}
+
+		return ret;
+	}
+
 
     public void ShowBox(string dialogue)
     {
@@ -52,8 +99,8 @@ public class DialogueManager : MonoBehaviour {
     {
         Debug.Log("Activating now");
         dialogueActive = true;
+		dialogueEnd = false;
         dBox.SetActive(true);
         player.frozen = true;
-        currentLine = 0;
     }
 }
