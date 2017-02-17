3a127ac8afe68a7910dafef54050bd6b785fd064
Reid Bixler <rmb3yz@virginia.edu>
2017-02-16 22:18:52 -0500

    Added Grass Tiles!
    Added some grass tiles for Tiled that 1) Don’t look like crap 2)
    Actually work 3) Have a lot of random possible middle tiles.

---
+++ Art/Tilesets/ReidArt/Grass-Tile-Full-Green.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Grass-Tile-Full-Green" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="Grass-Tile-Full-Green.png" width="320" height="320"/>
+</tileset>
