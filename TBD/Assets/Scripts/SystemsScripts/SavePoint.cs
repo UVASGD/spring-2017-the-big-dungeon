618261d5c0add55b8d1c74c81b3fb39cec7001ac
Soupdoop <jherd23@gmail.com>
2017-02-25 14:30:07 -0500

    Added Working Save Point Objects
    Note: No pop up window

--- TBD/Assets/Scripts/SystemsScripts/SavePoint.cs
+++ TBD/Assets/Scripts/SystemsScripts/SavePoint.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class SavePoint : MonoBehaviour {
 
+	PlayerController player;
+	SaveController sc;
+
+	bool withinRange;
+
 	// Use this for initialization
 	void Start () {
-		
+		player = FindObjectOfType<PlayerController> ();
+		sc = FindObjectOfType<SaveController> ();
+		withinRange = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (withinRange) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				sc.SaveTo ("default");
+				Debug.Log ("Saved");
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			withinRange = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			withinRange = false;
+		}
 	}
 }
