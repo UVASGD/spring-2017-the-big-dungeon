ec3ee7a31ddbbc8945e84a1cd751dd2750e0d532
willwill96 <wbw4sv@virginia.edu>
2017-03-01 23:10:08 -0500

    Added rain, fog and snow as options for weather

---
+++ TBD/Assets/Scripts/SystemsScripts/rainScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rainScript : MonoBehaviour {
+    public bool isActive;
+    private ParticleSystem pSys;
+	// Use this for initialization
+	void Start () {
+        isActive = false;
+        pSys = this.GetComponent<ParticleSystem>();
+        pSys.Stop();
+        pSys.Clear();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R))
+        {
+            isActive = !isActive;
+            if (isActive)
+            {
+                pSys.Play();
+            }
+            else
+            {
+                pSys.Stop();
+                pSys.Clear();
+            }
+        }
+	}
+}
