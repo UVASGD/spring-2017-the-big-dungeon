43c74287e23b9b8eb551e06e90722e374dc047f4
alamb93 <anl4ds@virginia.edu>
2017-03-08 01:24:15 -0500

    Added Basic Stat System

---
+++ TBD/Assets/Scripts/PlayerScripts/BaseStat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStat {
+
+    public int baseVal { get; set; }
+    public string statName { get; set; }
+    public string description { get; set; }
+    public int modifier { get; set; }
+    
+
+    public BaseStat(string statName, int baseVal, string description)
+    {
+        this.statName = statName;
+        this.baseVal = baseVal;
+        this.description = description;
+        modifier = 0;
+    }
+
+    public int finalValue()
+    {
+        return baseVal + modifier;
+    }
+
+}
