ad3a43e77770bf49c572543a25d38b666bc8175f
alamb93 <anl4ds@virginia.edu>
2017-03-08 01:24:15 -0500

    Added Basic Stat System

---
+++ TBD/Assets/Scripts/PlayerScripts/PlayerStats.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+	public List<BaseStat> stats = new List<BaseStat>();
+	// Use this for initialization
+	void Start () {
+		stats.Add(new BaseStat("strength", 10, "Damage Dealt"));
+		stats.Add(new BaseStat("defense", 11, "Damage Taken"));
+		stats.Add(new BaseStat("HP", 12, "Health"));
+    }
+
+    public int getStatValue(string statName)
+	{
+		foreach (BaseStat s in stats) {
+            if (String.Compare(s.statName, statName) == 0) 
+			{
+				return s.finalValue();
+			}
+		}
+        return 0;
+    }
+
+	public void modifyStat(string statName, int modifier)
+    {
+		foreach (BaseStat s in stats)
+        {
+			if (String.Compare(s.statName, statName) == 0)
+            {
+				s.modifier += modifier;
+            }
+        }
+    }
+}
