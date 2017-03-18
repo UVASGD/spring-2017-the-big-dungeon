a68b934757700aa3218eff688251d0ccc61b4f9a
Cyrus Malekpour <cmalekpour@gmail.com>
2017-03-18 16:37:54 -0400

    Temp commit

--- TBD/Assets/Scripts/CutsceneScripts/CutsceneManager.cs
+++ TBD/Assets/Scripts/CutsceneScripts/CutsceneManager.cs
@@ -3,23 +3,44 @@ using System.Collections;
 
 public class CutsceneManager : MonoBehaviour
 {
-
-	private Camera camera;
+	
+	private Camera mainCamera;
+	private CameraManager cameraManager;
+	private PlayerController player;
 
 	// Use this for initialization
 	void Start ()
 	{
-		camera = GetComponent<Camera>();
+		mainCamera = Camera.main;
+		cameraManager = FindObjectOfType<CameraManager>();
+		Debug.Log (cameraManager);
+		player = FindObjectOfType<PlayerController> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.L))
+		{ 
+			StartCutscene ();
+		}
 	}
 	
 	public void StartCutscene()
 	{
+		cameraManager.freeze = true;
+		player.frozen = true;
+
 		// The bulk of the work is done by the DialogueManager
 	}
 
 	public void EndCutscene()
 	{
+
+		mainCamera.ResetProjectionMatrix ();
+
 		// Clean-up and fix DialogueManager
+		cameraManager.freeze = false;
+		player.frozen = false;
 	}
 
 }
