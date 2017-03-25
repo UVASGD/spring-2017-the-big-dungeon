147aed0a5646669d491f35f55c377b478513bda5
Soupdoop <jherd23@gmail.com>
2017-03-25 01:08:03 -0400

    No Branches We Commit To Master
    Added moving object script

---
+++ TBD/Assets/Scripts/SystemsScripts/ObjectLock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectLock : MonoBehaviour {
+
+	public GameObject target;
+
+	bool locked;
+
+	// Use this for initialization
+	void Start () {
+		locked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!locked && ((Vector2)gameObject.transform.position - (Vector2)target.transform.position).magnitude < 0.05) {
+			Debug.Log ("FREEZE");
+			locked = true;
+			gameObject.transform.position = target.transform.position;
+			gameObject.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeAll;
+		}
+	}
+
+}
