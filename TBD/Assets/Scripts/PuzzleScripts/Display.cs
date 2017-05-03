aa57dd84c1fa4471040a2650bc2d171453eb051b
Soupdoop <jherd23@gmail.com>
2017-05-03 00:03:42 -0400

    Some Underwater ~~changes~~
    ~~~~~~ V A P O R W A V E ~~~~~~

---
+++ TBD/Assets/Scripts/PuzzleScripts/Display.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Display : MonoBehaviour {
+
+	public Sprite[] states;
+	public SpriteRenderer[] displayBois;
+	public int[] currentValues;
+	public int[] correctValues;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Evaluate ()) {
+			//Call thing that effects world.
+		}
+	}
+
+	public void Apply(int[] increments) {
+		if (states.Length > 0) {
+			for (int i = 0; i < currentValues.Length && i < increments.Length; i++) {
+				currentValues [i] = (currentValues [i] + increments [i]) % states.Length;
+			}
+			UpdateDisplay ();
+		}
+	}
+
+	public void UpdateDisplay() {
+		for (int i = 0; i < displayBois.Length && i < currentValues.Length; i++) {
+			displayBois [i].sprite = states [currentValues [i] % states.Length];
+		}
+	}
+
+	public bool Evaluate() {
+		for (int i = 0; i < currentValues.Length && i < correctValues.Length; i++) {
+			if (currentValues [i] != correctValues [i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ TBD/Assets/Scripts/PuzzleScripts/PuzzleButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Collider2D))]
+public class PuzzleButton : MonoBehaviour {
+
+	public int[] changes;
+	public Display disp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "player") {
+			disp.Apply (changes);
+		}
+	}
+}
